3de1542346758d62fc1c45ed2a361d218810bd0e
hina.maeyama <hina.maeyama@add-more.co.jp>
2020-09-16 21:26:21 +0900

    [add]hello app

---
+++ hello-world-app/src/HelloWorld.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function HelloWorld(){
+    return (
+        <div>
+        <h1>hello,world</h1>
+        <ul>
+            <li>hello</li>
+            <li>world</li>
+            <li>hello</li>
+            <li>world</li>
+        </ul>
+        </div>
+    )
+}
+
+export default HelloWorld;
