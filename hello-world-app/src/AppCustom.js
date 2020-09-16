625b199b4db7fb848e9b5416e306377f8540c61d
hina.maeyama <hina.maeyama@add-more.co.jp>
2020-09-16 21:34:29 +0900

    can render AppCustom

---
+++ hello-world-app/src/AppCustom.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './HelloWorld';
+
+function AppCustom(){
+
+    return(
+        <div className="App">
+            <HelloWorld />
+        </div>
+    )
+}
+
+
+export default AppCustom;
